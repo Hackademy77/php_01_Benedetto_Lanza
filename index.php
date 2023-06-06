cf93fe72cc0bb9832d19add2925ca00f54202607
Lanza Benedetto <bennylanza@gmail.com>
2023-06-06 16:49:54 +0200

    esercizio 3 finito

--- index.php
+++ index.php
@@ -65,5 +65,22 @@ Pushate su Github chiamando la repository php_01_nome_cognome e inviate il link 
 //   }
 // }
 
+//ESERCIZIO 3: scrivere un programma che stampi in console tutti i numeri da uno a cento. Se il numero è multiplo di 3 stampare “PHP” al posto del numero; se multiplo di 5 stampare “JAVASCRIPT”; se multiplo di 3 e 5 contemporaneamente deve stampare “HACKADEMY66".
+
+// for ($i=1; $i <= 100 ; $i++) { 
+//     if($i % 3 == 0 && $i % 5 == 0) {
+//         echo "HACKADEMY66 \n";
+//     }
+//     elseif ($i % 5 == 0) {
+//         echo "JAVASCRIPT \n";
+//     }
+//     elseif ($i % 3 == 0) {
+//         echo "PHP \n";
+//     }
+//     else {
+//         echo "$i \n";
+//     }
+// }
+
 
 ?>
